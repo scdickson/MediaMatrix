e73f367c59ab95fa4c3642d22621ab43e8e41a3a
Niraj Venkat <venkatn@purdue.edu>
2014-10-19 04:24:31 -0400

    Revert "Bug Fix"
    This reverts commit 9566d3cd1e38c66a831340e7c630399c402c1129.

--- app/src/main/java/scd/com/mediamatrix/MatrixInitialization.java
+++ app/src/main/java/scd/com/mediamatrix/MatrixInitialization.java
@@ -326,19 +326,13 @@ public class MatrixInitialization extends Activity
                     //Log.d("mm", dataSnapshot.getValue().toString());
                     if(b != null)
                     {
-                        try {
-                            String coords[] = dataSnapshot.getValue().toString().split(";");
-                            int x = Integer.parseInt(coords[0]);
-                            int y = Integer.parseInt(coords[1]);
-                            int height = Integer.parseInt(coords[2]);
-                            int width = Integer.parseInt(coords[3]);
-                            Bitmap croppedBitmap = Bitmap.createBitmap(b, x, y, width, height); //source, x, y, width, height
-                            fullscreenImage.setImageBitmap(croppedBitmap);
-                        }
-                        catch(Exception e)
-                        {
-                            e.printStackTrace();
-                        }
+                        String coords[] = dataSnapshot.getValue().toString().split(";");
+                        int x = Integer.parseInt(coords[0]);
+                        int y = Integer.parseInt(coords[1]);
+                        int height = Integer.parseInt(coords[2]);
+                        int width = Integer.parseInt(coords[3]);
+                        Bitmap croppedBitmap = Bitmap.createBitmap(b, x, y, width, height); //source, x, y, width, height
+                        fullscreenImage.setImageBitmap(croppedBitmap);
                     }
                 }
 
